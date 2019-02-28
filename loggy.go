779b71ef3b70a472631dc838d3b56eec941ccffc
Miraslau Kazak <m9kazak@gmail.com>
2019-02-28 16:59:12 -0600

    Completed package

--- loggy.go
+++ loggy.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"io"
 	"fmt"
+	"os"
 )
 
 // LogLevel type
@@ -11,7 +12,8 @@ type LogLevel int
 
 // Log Level
 const (
-	CriticalLevel LogLevel = iota + 1
+	FatalLevel LogLevel = iota + 1
+	PanicLevel
 	ErrorLevel
 	WarningLevel
 	InfoLevel
@@ -62,10 +64,92 @@ func (l *Logger) Debugln(a ...interface{}) {
 	l.internalLog(DebugLevel, true, fmt.Sprintln(a...))
 }
 
+// Info logs a message at Info level. Arguments are handled in the manner of fmt.Print
+func (l *Logger) Info(a ...interface{}) {
+	l.internalLog(InfoLevel, false, fmt.Sprint(a...))
+}
+
+// Infof logs a message at Info level. Arguments are handled in the manner of fmt.Printf
+func (l *Logger) Infof(format string, a ...interface{}) {
+	l.internalLog(InfoLevel, false, fmt.Sprintf(format, a...))
+}
+
+// Infoln logs a message at Info level. Arguments are handled in the manner of fmt.Println
+func (l *Logger) Infoln(a ...interface{}) {
+	l.internalLog(InfoLevel, true, fmt.Sprintln(a...))
+}
+
+// Warning logs a message at Warning level. Arguments are handled in the manner of fmt.Print
+func (l *Logger) Warning(a ...interface{}) {
+	l.internalLog(WarningLevel, false, fmt.Sprint(a...))
+}
+
+// Warningf logs a message at Warning level. Arguments are handled in the manner of fmt.Printf
+func (l *Logger) Warningf(format string, a ...interface{}) {
+	l.internalLog(WarningLevel, false, fmt.Sprintf(format, a...))
+}
+
+// Warningln logs a message at Warning level. Arguments are handled in the manner of fmt.Println
+func (l *Logger) Warningln(a ...interface{}) {
+	l.internalLog(WarningLevel, true, fmt.Sprintln(a...))
+}
+
+// Error logs a message at Error level. Arguments are handled in the manner of fmt.Print
+func (l *Logger) Error(a ...interface{}) {
+	l.internalLog(ErrorLevel, false, fmt.Sprint(a...))
+}
+
+// Errorf logs a message at Error level. Arguments are handled in the manner of fmt.Printf
+func (l *Logger) Errorf(format string, a ...interface{}) {
+	l.internalLog(ErrorLevel, false, fmt.Sprintf(format, a...))
+}
+
+// Errorln logs a message at Error level. Arguments are handled in the manner of fmt.Println
+func (l *Logger) Errorln(a ...interface{}) {
+	l.internalLog(ErrorLevel, true, fmt.Sprintln(a...))
+}
+
+// Panic logs a message at Panic level. Arguments are handled in the manner of fmt.Print
+func (l *Logger) Panic(a ...interface{}) {
+	l.internalLog(PanicLevel, false, fmt.Sprint(a...))
+	panic(fmt.Sprint(a...))
+}
+
+// Panicf logs a message at Panic level. Arguments are handled in the manner of fmt.Printf
+func (l *Logger) Panicf(format string, a ...interface{}) {
+	l.internalLog(PanicLevel, false, fmt.Sprintf(format, a...))
+	panic(fmt.Sprintf(format, a...))
+}
+
+// Panicln logs a message at Panic level. Arguments are handled in the manner of fmt.Println
+func (l *Logger) Panicln(a ...interface{}) {
+	l.internalLog(PanicLevel, true, fmt.Sprintln(a...))
+	panic(fmt.Sprint(a...))
+}
+
+// Fatal logs a message at Fatal level. Arguments are handled in the manner of fmt.Print
+func (l *Logger) Fatal(a ...interface{}) {
+	l.internalLog(FatalLevel, false, fmt.Sprint(a...))
+	os.Exit(1)
+}
+
+// Fatal logs a message at Fatal level. Arguments are handled in the manner of fmt.Printf
+func (l *Logger) Fatalf(format string, a ...interface{}) {
+	l.internalLog(FatalLevel, false, fmt.Sprintf(format, a...))
+	os.Exit(1)
+}
+
+// Fatalln logs a message at Fatal level. Arguments are handled in the manner of fmt.Println
+func (l *Logger) Fatalln(a ...interface{}) {
+	l.internalLog(FatalLevel, true, fmt.Sprintln(a...))
+	os.Exit(1)
+}
+
 // Return LogLeve as a string
 func getLevelString (level LogLevel) string {
 	logLevels := [...]string{
-		"[CRITICAL]",
+		"[FATAL]",
+		"[PANIC]",
 		"[ERROR]",
 		"[WARNING]",
 		"[INFO]",
